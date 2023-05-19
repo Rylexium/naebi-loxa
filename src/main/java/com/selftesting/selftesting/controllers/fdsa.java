0dbc3eba2a08396e81ed0bc2e856142b41a10929
Rylexium <aleksei.azizov@mail.ru>
2023-05-19 23:23:27 +0400

    change

--- src/main/java/com/selftesting/selftesting/controllers/fdsa.java
+++ src/main/java/com/selftesting/selftesting/controllers/fdsa.java
@@ -4,7 +4,8 @@ public class fdsa {
   public static void main(String[] args) {
      System.out.println("Lera privet");
       System.out.println("Ksixa privet");
-      System.out.println("Ilya naebal loxa");
-      System.out.println("Ilya naebal loxa");
+      System.out.println("Ilya naebal loxa1");
+      System.out.println("Ilya naebal loxa2");
+      System.out.println("Ilya naebal loxa3");
   }
 }
