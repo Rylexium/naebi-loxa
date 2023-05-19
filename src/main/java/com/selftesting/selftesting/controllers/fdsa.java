16a4d88dae088ab1614abb4ee5ffaf9e1ee03010
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-05-19 20:38:02 +0400

    Ilay privet

--- src/main/java/com/selftesting/selftesting/controllers/fdsa.java
+++ src/main/java/com/selftesting/selftesting/controllers/fdsa.java
@@ -5,5 +5,6 @@ public class fdsa {
      System.out.println("Lera privet");
       System.out.println("Ksixa privet");
        System.out.println("Lexa privet");
+           System.out.println("Ilay privet");
   }
 }
