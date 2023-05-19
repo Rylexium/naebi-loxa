1a6e942b37363a5728dffaba4cb5a3a992e81086
Rylexium <aleksei.azizov@mail.ru>
2023-05-19 20:35:23 +0400

    add Ksixa

--- src/main/java/com/selftesting/selftesting/controllers/fdsa.java
+++ src/main/java/com/selftesting/selftesting/controllers/fdsa.java
@@ -2,6 +2,7 @@ package com.selftesting.selftesting.controllers;
 
 public class fdsa {
   public static void main(String[] args) {
-   System.out.println("Lera privet");
+     System.out.println("Lera privet");
+      System.out.println("Ksixa privet");
   }
 }
