246e65da5d27770103f53614f98c8ccbdfa7b2da
Rylexium <84678136+Rylexium@users.noreply.github.com>
2023-05-19 20:33:01 +0400

    Update fdsa.java

--- src/main/java/com/selftesting/selftesting/controllers/fdsa.java
+++ src/main/java/com/selftesting/selftesting/controllers/fdsa.java
@@ -3,5 +3,6 @@ package com.selftesting.selftesting.controllers;
 public class fdsa {
   public static void main(String[] args) {
    System.out.println("Lera privet");
+       System.out.println("Lexa privet");
   }
 }
